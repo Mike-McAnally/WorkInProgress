155a0db9d97f1c55df91909e25b05ae561050320
MBlade <mwmcanally@gmail.com>
2019-10-15 17:23:19 -0700

    checked-in latest files without videos

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 Virtual Construct Basic WebVR Node Network Structure "Mind Palace" project.
 
 https://www.youtube.com/watch?v=QrNP_QLdaRM&feature=youtu.be
+
+# How to run
+1. Install NodeJS and NPM
+* https://nodejs.org/en
+
+2. Install modules needed to run node server
+* `npm install`
+
+3. Run node app
+* `npm start`
+
+4. Connect to http://localhost:3025/VRnodeLabelA5.html with multiple devices
---
+++ index.js
@@ -0,0 +1,72 @@
+// Setting up a basic express server
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+
+// Modify listening port value if needed
+const PORT = 3025;
+const HOST = '0.0.0.0';
+// Configuring easyrtc server for NAF
+const webServer = require('http').Server(app);
+const socketServer = require('socket.io')(webServer);
+const easyrtc = require('easyrtc');
+const myIceServers = [
+    { 'url': 'stun:stun.l.google.com:19302' },
+    { 'url': 'stun:stun1.l.google.com:19302' },
+    { 'url': 'stun:stun2.l.google.com:19302' },
+    { 'url': 'stun:stun3.l.google.com:19302' },
+];
+
+easyrtc.setOption('appIceServers', myIceServers);
+easyrtc.setOption('logLevel', 'debug');
+easyrtc.setOption('demosEnable', false);
+
+// easyrtc methods override; pre-made code from https://github.com/networked-aframe/networked-aframe/blob/master/server/easyrtc-server.js
+easyrtc.events.on("easyrtcAuth", function (socket, easyrtcid, msg, socketCallback, callback) {
+    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function (err, connectionObj) {
+        if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
+            callback(err, connectionObj);
+            return;
+        }
+
+        connectionObj.setField("credential", msg.msgData.credential, { "isShared": false });
+
+        console.log("[" + easyrtcid + "] Credential saved!", connectionObj.getFieldValueSync("credential"));
+
+        callback(err, connectionObj);
+    });
+});
+
+// To test, lets print the credential to the console for every room join!
+easyrtc.events.on("roomJoin", function (connectionObj, roomName, roomParameter, callback) {
+    console.log("[" + connectionObj.getEasyrtcid() + "] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
+    easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
+});
+
+// Start EasyRTC server
+easyrtc.listen(app, socketServer, null, function (err, rtcRef) {
+    console.log("Initiated");
+
+    rtcRef.events.on("roomCreate", function (appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+        console.log("roomCreate fired! Trying to create: " + roomName);
+
+        appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
+    });
+});
+
+function run() {
+    // Return HTML pages in public directory when path matches
+    app.use('/', express.static(path.join(__dirname, './public')));
+
+    webServer.listen(PORT, HOST, () => {
+        console.log(`app running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = run;
+
+if (require.main === module) {
+    run();
+}
