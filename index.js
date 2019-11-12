5f1d36bd36f5a57ec5b7536631ef9e79a20be365
avasconcelos114 <andre.onogoro@gmail.com>
2019-11-12 14:00:37 +0900

    Improvements to audio and pdf reader

--- index.js
+++ index.js
@@ -1,90 +1,94 @@
-// Setting up a basic express server
-const express = require('express');
+// Load required modules
+const http = require("http");
+const express = require("express");
+const serveStatic = require('serve-static');
+const socketIo = require("socket.io");
+const easyrtc = require("easyrtc");
 const path = require('path');
-const app = express();
-
-const fs = require('fs');
-const schedule = require('node-schedule');
 const PDF2Pic = require("pdf2pic");
 
+// Set process name
+process.title = "node-easyrtc";
+
+// Get port or default to 3025
+var port = process.env.PORT || 3025;
+
+// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
+var app = express();
 app.use(express.json());
+app.use(serveStatic('public_html', {'index': ['Islands.html']}));
 
-// Modify listening port value if needed
-const PORT = 3025;
-const HOST = '0.0.0.0';
-// Configuring easyrtc server for NAF
-const webServer = require('http').Server(app);
-const socketServer = require('socket.io')(webServer);
-const easyrtc = require('easyrtc');
-const myIceServers = [
-    { 'url': 'stun:stun.l.google.com:19302' },
-    { 'url': 'stun:stun1.l.google.com:19302' },
-    { 'url': 'stun:stun2.l.google.com:19302' },
-    { 'url': 'stun:stun3.l.google.com:19302' },
+// Start Express http server
+var webServer = http.createServer(app);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = socketIo.listen(webServer, {"log level":1});
+
+var myIceServers = [
+  {"url":"stun:stun.l.google.com:19302"},
+  {"url":"stun:stun1.l.google.com:19302"},
+  {"url":"stun:stun2.l.google.com:19302"},
+  {"url":"stun:stun3.l.google.com:19302"}
 ];
+easyrtc.setOption("appIceServers", myIceServers);
+easyrtc.setOption("logLevel", "debug");
+easyrtc.setOption("demosEnable", false);
 
-easyrtc.setOption('appIceServers', myIceServers);
-easyrtc.setOption('logLevel', 'debug');
-easyrtc.setOption('demosEnable', false);
-
-// easyrtc methods override; pre-made code from https://github.com/networked-aframe/networked-aframe/blob/master/server/easyrtc-server.js
-easyrtc.events.on("easyrtcAuth", function (socket, easyrtcid, msg, socketCallback, callback) {
-    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function (err, connectionObj) {
+// Overriding the default easyrtcAuth listener, only so we can directly access its callback
+easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback) {
+    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function(err, connectionObj){
         if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
             callback(err, connectionObj);
             return;
         }
 
-        connectionObj.setField("credential", msg.msgData.credential, { "isShared": false });
+        connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
 
-        console.log("[" + easyrtcid + "] Credential saved!", connectionObj.getFieldValueSync("credential"));
+        console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
 
         callback(err, connectionObj);
     });
 });
 
 // To test, lets print the credential to the console for every room join!
-easyrtc.events.on("roomJoin", function (connectionObj, roomName, roomParameter, callback) {
-    console.log("[" + connectionObj.getEasyrtcid() + "] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
+easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
+    console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
     easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
 });
 
 // Start EasyRTC server
-easyrtc.listen(app, socketServer, null, function (err, rtcRef) {
+var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
     console.log("Initiated");
 
-    rtcRef.events.on("roomCreate", function (appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+    rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
         console.log("roomCreate fired! Trying to create: " + roomName);
 
         appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
     });
 });
 
+/*
+ *  PDF API
+ */
 app.get(/(.*\.pdf)\/([0-9]+)$/i, async function (req, res) {
     try {
         const pdfPath = path.join(__dirname, `./public_html/assets/pdf/${req.params[0]}`);
-        const pageNumber = req.params[1];
+        const fileName = req.params[0].substring(1, req.params[0].length - 4)
     
         const pdf2pic = new PDF2Pic({
             density: 100,
-            savename: `${req.params[0]}-${pageNumber}`,
-            savedir: "./public_html/assets/tmp", 
+            savename: fileName,
+            savedir: `./public_html/assets/tmp/${fileName}`, 
             format: "png",   
             size: "1190x1684"
         });
         
-        const file = await pdf2pic.convert(pdfPath, pageNumber)
-        if (!file.name) {
-            res.status(500).send('Could not convert file');
-            return;
-        }
+        const files = await pdf2pic.convertBulk(pdfPath, -1)
         
         const response = {
-            path: `/assets/assets/tmp/${file.name}`
+            pages: files.length,
+            files
         }
-
-        // deletes file created after 24 hours
-        scheduleJob(86400000, deleteFileIfExists, path.join(__dirname, `./public_html/assets/tmp/${file.name}`));
         res.send(response);
     } catch(e) {
         console.log(e);
@@ -92,32 +96,7 @@ app.get(/(.*\.pdf)\/([0-9]+)$/i, async function (req, res) {
     }
 });
 
-function scheduleJob(time, callback, args) {
-    const startTime = new Date(Date.now() + time);
-    schedule.scheduleJob(startTime, function() {
-        callback(args);
-    });
-}
-
-function deleteFileIfExists(path) {
-    if(fs.existsSync(path)) {
-        console.log('Deleting file in path:')
-        console.log(path);
-        fs.unlinkSync(path);
-    }
-}
-
-function run() {
-    // Return HTML pages in public directory when path matches
-    app.use('/assets/', express.static(path.join(__dirname, './public_html')));
-
-    webServer.listen(PORT, HOST, () => {
-        console.log(`app running on http://localhost:${PORT}`);
-    });
-}
-
-module.exports = run;
-
-if (require.main === module) {
-    run();
-}
+//listen on port
+webServer.listen(port, function () {
+  console.log('listening on http://localhost:' + port);
+});
